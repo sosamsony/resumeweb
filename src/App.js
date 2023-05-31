81fda207edbb68fb5a8242b1e94de8eb13123bd5
Samson Sanyaolu <sosarenaint@gmail.com>
2023-05-31 13:00:12 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Pages } from "./pages/Pages"
+import "./scss/main.scss"
+
+const App = () => {
+  return (
+    <>
+      <Pages />
+    </>
+  )
+}
+export default App
